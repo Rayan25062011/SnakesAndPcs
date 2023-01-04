52c726237690c2b4290857ce3c6f429a21abbfc5
Rayan25062011 <101386337+Rayan25062011@users.noreply.github.com>
2023-01-04 12:35:13 +0200

    Create App.tsx

---
+++ user/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>User's guide</h1>
+      <p>
+      1. Up arrow to go up
+      2. down arrow to go down
+      3. right arrow to go right
+      4. left arrow to go left
+        
+      !5! Restart app to play again
+      </p>
+    </div>
+  );
+}
